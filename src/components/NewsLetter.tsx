7325b4f7d3fc03e70d319da38a44168e70c0ef6c
Allan Aoudji <allanaoudji@air-de-allan.home>
2023-09-12 14:35:43 +0200

    style newsletter sm

--- src/components/NewsLetter.tsx
+++ src/components/NewsLetter.tsx
@@ -3,41 +3,45 @@
 import { Form } from '@/lib/enums';
 import { useSubscribeToNewsletter } from '@src/hooks/useSubscribeToNewsletter';
 import { bodoniModa } from '@src/utils/fonts';
+import Wrapper from './Wrapper';
 
 function NewsLetter() {
   const { form, inputEl, subscribe } = useSubscribeToNewsletter();
 
   return (
-    <section className="bg-secondary px-3 pt-16 pb-8">
-      <h4 className={`pb-10 text-4xl text-center ${bodoniModa.className}`}>
+    <Wrapper backgroundColor="darker" className="py-12">
+      <h4
+        className={`pb-10 text-4xl uppercase text-dark ${bodoniModa.className}`}
+      >
         Abonnez-vous à la newsletter
       </h4>
-      <form className="flex gap-2 mx-auto max-w-lg" onSubmit={subscribe}>
-        <div className="grow">
-          <input
-            autoComplete="email"
-            className="bg-secondary border-2 border-light border-opacity-50 duration-300 h-12 outline-none pl-4 text-dark transition-all w-full focus:border-opacity-100 focus:text-dark placeholder:opacity-50 placeholder:text-light"
-            placeholder="votre adresse e-mail"
-            ref={inputEl}
-            required
-            type="email"
-          />
-          <div
-            className={`flex font-bold h-12 items-center ${
-              form.state === Form.Error ? 'text-red-600' : 'text-green-600'
-            }`}
-          >
-            {form.message && <span>{form.message}</span>}
-          </div>
+      <form
+        className="grid grid-cols-6 gap-x-2 gap-y-4 mx-auto max-w-lg"
+        onSubmit={subscribe}
+      >
+        <input
+          autoComplete="email"
+          className="col-span-6 bg-dark border-2 text-lg font-bold border-lighter border-opacity-50 duration-300 h-12 outline-none pl-4 text-lighter transition-all focus:border-opacity-100 placeholder:opacity-50 placeholder:text-lighter placeholder:font-normal"
+          placeholder="votre adresse e-mail"
+          ref={inputEl}
+          required
+          type="email"
+        />
+        <div
+          className={`col-span-6 flex font-bold items-center ${
+            form.state === Form.Error ? 'text-light' : 'text-green-600'
+          }`}
+        >
+          {form.message && <span>{form.message}</span>}
         </div>
         <button
-          className="bg-light h-12 px-4 rounded-full uppercase text-dark"
+          className="col-span-4 col-start-2 bg-dark h-12 px-4 rounded-full uppercase text-darker"
           type="submit"
         >
           {form.state === Form.Loading ? 'loading...' : "S'abonner"}
         </button>
       </form>
-    </section>
+    </Wrapper>
   );
 }
 
