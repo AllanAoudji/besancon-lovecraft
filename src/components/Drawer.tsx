dce3eb6d9cbb4811964075c324f858f47a654976
Allan Aoudji <allanaoudji@air-de-allan.home>
2023-09-13 19:10:50 +0200

    finish drawer

--- src/components/Drawer.tsx
+++ src/components/Drawer.tsx
@@ -11,17 +11,17 @@ type Props = {
 function Drawer({ open }: Props) {
   return (
     <div
-      className={`absolute bg-secondary duration-700 ease-in-out flex flex-col h-full p-4 top-0 transition-all w-3/4 ${
+      className={`absolute bg-darker font-bold duration-700 ease-in-out flex flex-col h-full py-6 px-4 top-0 transition-all w-3/4 ${
         open ? 'left-0 opacity-100' : '-left-full opacity-0'
       }`}
     >
       <div className="flex items-center justify-between">
         <Logo className="h-10" />
-        <DrawerCloseLink />
+        <DrawerCloseLink className="h-6" />
       </div>
-      <Pages className="flex-col font-bold gap-1 pt-8 text-light text-xl [&_a]:border-b-2 [&_a]:border-light [&_a]:border-opacity-60 [&_a]:pb-2 [&_a]:pt-3 first:[&_a]:border-t-2" />
-      <SocialMedias className="flex-col grow pt-4 text-light text-xl [&_a]:py-3" />
-      <Copyright className="text-light" />
+      <Pages className="flex-col gap-1 pt-16 text-lighter text-xl [&_a]:border-b-2 [&_a]:border-lighter [&_a]:border-opacity-40 [&_a]:pb-3 [&_a]:pt-4 first:[&_a]:border-t-2" />
+      <SocialMedias className="flex-col grow pt-4 text-lighter text-xl [&_a]:py-3" />
+      <Copyright className="text-lighter [&_a]:border-lighter" />
     </div>
   );
 }
