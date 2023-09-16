c4ef4c3ea2fe9f93dd5824b89b344541226b7694
Allan Aoudji <allanaoudji@air-de-allan.home>
2023-09-16 21:50:27 +0200

    clean unused fonts

--- src/utils/fonts.ts
+++ src/utils/fonts.ts
@@ -1,13 +1,3 @@
-import {
-  Rubik,
-  Bodoni_Moda,
-  Libre_Bodoni,
-  Inter,
-  Playfair_Display,
-} from 'next/font/google';
+import { Playfair_Display } from 'next/font/google';
 
-export const rubik = Rubik({ subsets: ['latin'] });
-export const bodoniModa = Bodoni_Moda({ subsets: ['latin'] });
-export const libreBodoni = Libre_Bodoni({ subsets: ['latin'] });
-export const inter = Inter({ subsets: ['latin'] });
 export const playfair = Playfair_Display({ subsets: ['latin'] });
