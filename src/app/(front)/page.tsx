deec70b7fb833f6766d6753907527285781a6300
Allan Aoudji <allanaoudji@air-de-allan.home>
2023-09-23 00:38:34 +0200

    fixe les derniers articles y spacing

--- src/app/(front)/page.tsx
+++ src/app/(front)/page.tsx
@@ -45,7 +45,7 @@ export default async function Home({ searchParams: { drawer } }: Props) {
     >
       {!!posts.length && (
         <>
-          <h3 className="font-black pb-20 pt-24 text-3xl text-center text-darker uppercase lg:pb-24 lg:pt-28 lg:text-4xl">
+          <h3 className="font-black pb-24 pt-20 text-3xl text-center text-darker uppercase lg:pb-28 lg:pt-24 lg:text-4xl">
             Les derniers articles
           </h3>
           <Grid className="pb-12 sm:pb-16 lg:pb-24">
