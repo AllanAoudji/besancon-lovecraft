7a1abda97be1dfc425ae14412f6c06a2dff793c7
Allan Aoudji <allanaoudji@air-de-allan.home>
2023-10-02 15:04:42 +0200

    update image unoptimized config to only set this to instagram images

--- next.config.js
+++ next.config.js
@@ -14,12 +14,12 @@ const nextConfig = {
         protocol: 'https',
         hostname: '*.cdninstagram.com',
         port: '',
+        unoptimized: true,
       },
     ],
     // Besoin de cette ligne pour ne pas passer à la caisse.
     // https://vercel.com/docs/image-optimization/limits-and-pricing
     // https://vercel.com/docs/image-optimization/managing-image-optimization-costs
-    unoptimized: true,
   },
 };
 
