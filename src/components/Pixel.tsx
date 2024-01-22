0d80e7242bd682fee19c879d8125a6e1ea6ace2c
Allan Aoudji <allanaoudji@air-de-allan.home>
2024-01-22 20:15:34 +0100

    go back to previous pixel

--- src/components/Pixel.tsx
+++ src/components/Pixel.tsx
@@ -6,7 +6,7 @@ import { Helmet } from 'react-helmet';
 const Pixel = () => {
   return (
     <Helmet>
-      <script id="facebook-pixel-script">
+      <script>
         {`!function(f,b,e,v,n,t,s)
         {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
         n.callMethod.apply(n,arguments):n.queue.push(arguments)};
@@ -15,10 +15,10 @@ const Pixel = () => {
         t.src=v;s=b.getElementsByTagName(e)[0];
         s.parentNode.insertBefore(t,s)}(window, document,'script',
         'https://connect.facebook.net/en_US/fbevents.js');
-        fbq('init', '1453439188887645');
+        fbq('init', '910247493884693');
         fbq('track', 'PageView');`}
       </script>
-      <noscript id="facebook-pixel-image">
+      <noscript>
         {`<img height="1" width="1" style="display:none"
         src="https://www.facebook.com/tr?id=1453439188887645&ev=PageView&noscript=1"
         />`}
@@ -28,3 +28,5 @@ const Pixel = () => {
 };
 
 export default Pixel;
+
+//EAAFBWPs0LcMBO2uoqpQFrRdSZCtWmwAHfC5W3aTiuWcORtouY1saZBXR3EqtaKj7QGZBwQQkzOOZBu4uiek6tryzD9YvUZCtKbiSLykLPXw8ZAjOYllN3xw83QDiikTCTuPuZC1Qs892EN7BrTMVbPBjPNj2weZCwHBsnNZB8olgOck6k9dEkXOKeEZABygEZBfnlFzkQZDZD
